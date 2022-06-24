2dcc5185f5380c87c07a51c20ea098d460227a24
GrandfatherPurple <cldoebbert@gmail.com>
2022-06-24 16:28:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -79,11 +79,6 @@ const particlesOptions = {
 }
 
 const particlesInit = async (main) => {
-  // console.log(main);
-
-  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-  // starting from v2 you can add only the features you need reducing the bundle size
   await loadFull(main);
 };
 
@@ -124,12 +119,6 @@ class App extends Component {
     console.log('loadUser', this.state.user.name)
   }
 
-  // componentDidMount(){
-  //   fetch('http://localhost:4000')
-  //   .then(response => response.json())
-  //   .then(console.log)
-  // }
-
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -144,7 +133,6 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    // console.log(box);
     this.setState({box: box});
   }
 
